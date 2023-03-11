084371e8b83d58e0dd4d9cf4b2ada6939ac0632f
pbarzaghi <pablobarzaghi@gmail.com>
2023-03-10 22:43:54 -0300

    Proytecto Clase 4 Vacio

---
+++ ProyectoClase4/src/main/java/com/mycompany/proyectoclase4/ProyectoClase4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoclase4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoClase4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
