08412b43e3811543ae8bd72d4694f8d31ededffc
pbarzaghi <pablobarzaghi@gmail.com>
2023-03-10 23:06:54 -0300

    Clase 4 Ejercicio 1

---
+++ ProyectoClase4/src/main/java/com/mycompany/proyectoclase4/Clase4Ejercicio1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoclase4;
+
+/**
+ * a. haga un main, donde por parámetro ponga 3 números y una letra que
+ *     represente ascendente o descendente y los muestre ordenados por tal criterio
+ * @author pBatzaghi
+ */
+public class Clase4Ejercicio1 {
+  
+    
+    
+    public int[] imprimirOrdenArreglo(int[] numeros, char orden){
+   
+       int auxiliar;
+       boolean desendiente =(orden =='d');
+       for(int i=1; i< numeros.length;){
+       
+           if(desendiente){
+             if(numeros[i-1]> numeros[i]){
+              auxiliar=numeros[i-1];
+              numeros[i-1]=numeros[i];
+              numeros[i]=auxiliar;
+              i=1;
+             }else {
+              i++;
+             }
+           }else {
+                 if(numeros[i-1]  < numeros[i]){
+                    auxiliar=numeros[i];
+                    numeros[i]=numeros[i-1];
+                    numeros[i-1]=auxiliar;
+                      i=1;
+                  }else {
+                      i++;
+                  }//Fin If
+           
+           }// fin else
+       
+       }// fin del for
+       return numeros;
+   }  
+}
--- ProyectoClase4/src/main/java/com/mycompany/proyectoclase4/ProyectoClase4.java
+++ ProyectoClase4/src/main/java/com/mycompany/proyectoclase4/ProyectoClase4.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.proyectoclase4;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
@@ -11,6 +13,31 @@ package com.mycompany.proyectoclase4;
 public class ProyectoClase4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Ejercicio 1
+        ejercicio1();
+    }
+    
+    
+    private static void ejercicio1(){
+    int[] numeros= new int[3];
+   
+    String nro1=JOptionPane.showInputDialog("Ingrese un munero 1 ");
+    String nro2=JOptionPane.showInputDialog("Ingrese un munero 2 ");
+    String nro3=JOptionPane.showInputDialog("//Ingrese un munero 3 ");
+    
+    numeros[0]= Integer.parseInt(nro1);
+    numeros[1]= Integer.parseInt(nro2);
+    numeros[2]= Integer.parseInt(nro3);
+    
+ 
+    
+     char orden =  JOptionPane.showInputDialog("Order Desendiente (D)- Ascendiente (A)").toLowerCase().charAt(0);
+   
+     System.out.println("INGRESO Nros Originales " +nro1+" "+ nro2+" "+nro3);
+     Clase4Ejercicio1 ejemplo1=new  Clase4Ejercicio1();
+     numeros=ejemplo1.imprimirOrdenArreglo(numeros, orden);
+     System.out.println("Orden "+orden + " Vector "+numeros[0]+" "+numeros[1]+" "+numeros[2]);
+    
+    
     }
 }
